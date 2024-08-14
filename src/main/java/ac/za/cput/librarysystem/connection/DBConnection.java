960c6664eeacc93d4ddb7fa4391d98aa207b2c04
Irishtheboy <francolukhele14@gmail.com>
2024-08-14 14:17:37 +0200

    added new classes that deal with the database

---
+++ src/main/java/ac/za/cput/librarysystem/connection/DBConnection.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ac.za.cput.librarysystem.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Sabura11
+ */
+public class DBConnection {
+    public static Connection derbyConnection() throws SQLException{
+     String dbURL = "jdbc:derby://localhost:1527/University";
+        String username = "administrator";
+        String password = "admin";
+    
+        Connection connection = DriverManager.getConnection(dbURL,username, password);
+        return connection;
+    }
+    
+}
