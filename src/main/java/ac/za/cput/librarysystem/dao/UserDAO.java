894dffa56c6abd3a09f90f515a43bbfcbfc45ebd
Oratile207 <161473496+Oratile207@users.noreply.github.com>
2024-08-19 01:11:56 +0200

    Update UserDAO.java

--- src/main/java/ac/za/cput/librarysystem/dao/UserDAO.java
+++ src/main/java/ac/za/cput/librarysystem/dao/UserDAO.java
@@ -15,8 +15,84 @@ import javax.swing.JOptionPane;
 
 /**
  *
- * @author Sabura11
+ * @authors Sabura11, oratile
  */
 public class UserDAO {
+    private Connection con;
+    private Statement stmt;
+    private PreparedStatement pstmt;
+    
+    
+    public UserDAO() {
+        try {
+            this.con = DBConnection.derbyConnection();
+            System.out.println("Connection made");
+        }
+        catch(SQLException exception){
+            JOptionPane.showMessageDialog(null, exception.getMessage(), "Warning", 
+                    JOptionPane.ERROR_MESSAGE);
+        }
+    }
 
+    // Sign up (Create a new user)
+     public void signUp(User user){
+        String sql = "INSERT INTO CLIENT (USER_NAME, EMAIL, PHONE, PASSWORD) VALUES(?, ?, ?, ?)";
+        try {
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, user.getUser_Name());
+            pstmt.setString(2, user.getUserEmail());
+            pstmt.setString(3, user.getUserPhone());
+            pstmt.setString(4, user.getUserPassword());
+            pstmt.executeUpdate();
+        }
+        catch(SQLException sqlException) {
+          JOptionPane.showMessageDialog(null, "SQL Error: " + sqlException.getMessage());
+            }
+        catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error: " + e.getMessage());
+            }
+        finally {
+            try {
+                if (stmt != null)
+                    stmt.close();
+
+            }
+            catch(SQLException exception){
+                JOptionPane.showMessageDialog(null, exception.getMessage(), "Warning", JOptionPane.ERROR_MESSAGE);
+            }
+        }//end finally
+    }//end method
+
+     // Login (Verify user credentials)
+     public void login(User user){
+         String sql = "SELECT * FROM CLIENT WHERE USER_NAME = ? AND PASSWORD = ?";
+         try{
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, user.getUser_Name());
+            pstmt.setString(2, user.getUserPassword());
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                 JOptionPane.showMessageDialog(null,"Login successful");
+            } else {
+                 JOptionPane.showMessageDialog(null,"Invalid username or password");
+            }
+        } 
+         catch(SQLException sqlException) {
+          JOptionPane.showMessageDialog(null, "SQL Error: " + sqlException.getMessage());
+            }
+//        catch (Exception e) {
+//            JOptionPane.showMessageDialog(null, "Error: " + e.getMessage());
+//            }
+         
+         finally {
+            try{
+            if (pstmt != null) {
+                pstmt.close();
+                }
+            }
+         catch(SQLException exception){
+                JOptionPane.showMessageDialog(null, exception.getMessage(), "Warning", JOptionPane.ERROR_MESSAGE);
+            }
+         }
+     }//end method
 }
